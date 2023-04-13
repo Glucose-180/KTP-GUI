883a2c5ac47215a19f867ea85cc6d7ebfa300e81
Glucose-180 <Glucose180@163.com>
2023-04-13 18:55:33 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(83, 54)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(347, 30)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(436, 45)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 48)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "w"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(436, 195)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(200, 48)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "r"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 24
+        Me.ListBox1.Location = New System.Drawing.Point(83, 176)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(347, 244)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(714, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.IO.MemoryMappedFiles
+Public Class Form1
+    Dim SmemIsOpen As Boolean
+    Dim mmf_cmd, mmf_dat As MemoryMappedFile
+    Dim macc_cmd, macc_dat As MemoryMappedViewAccessor
+    Const DSIZE = 64
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SmemIsOpen = False
+    End Sub
+
+    Public Sub Smem_Open(ByVal Name_cmd As String, ByVal Name_dat As String)
+        Try
+            mmf_cmd = MemoryMappedFile.OpenExisting(Name_cmd)
+            mmf_dat = MemoryMappedFile.OpenExisting(Name_dat)
+            macc_cmd = mmf_cmd.CreateViewAccessor
+            macc_dat = mmf_dat.CreateViewAccessor
+            SmemIsOpen = True
+        Catch ex As Exception
+            MsgBox("打开内存映射文件失败：" & vbCrLf & ex.ToString, 16, "Error")
+            SmemIsOpen = False
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If SmemIsOpen = False Then
+            Call Smem_Open("ktp_cmd_g", "ktp_dat_g")
+        End If
+        If SmemIsOpen = True Then
+            WriteCmd(TextBox1.Text)
+        End If
+    End Sub
+
+    Public Sub Smem_Close()
+        If SmemIsOpen = True Then
+            mmf_cmd.Dispose()
+            mmf_dat.Dispose()
+            SmemIsOpen = False
+        End If
+    End Sub
+
+    Public Sub WriteCmd(ByRef cmd As String)
+        Dim i As Integer
+        For i = 0 To cmd.Length - 1
+            macc_cmd.Write(Of Byte)(i, AscW(cmd.ElementAt(i)))
+        Next
+        macc_cmd.Write(Of Byte)(i + 1, 0)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim data(DSIZE) As Int32
+        Dim i As Int32
+        If SmemIsOpen = False Then
+            MsgBox("错误：内存映射文件未打开！", 48, "Error")
+            Exit Sub
+        End If
+        TextBox1.Text = ReadCmd(DSIZE)
+        data = ReadData(DSIZE)
+        ListBox1.Items.Clear()
+        For i = 0 To DSIZE - 1
+            ListBox1.Items.Add(data(i) \ 100 & vbTab & data(i) Mod 100)
+        Next
+    End Sub
+
+    Public Function ReadCmd(ByVal Size As Integer) As String
+        Dim cmd As String
+        Dim buf(Size) As Byte
+        Dim i As Integer
+        cmd = ""
+        Try
+            macc_cmd.ReadArray(Of Byte)(0, buf, 0, Size)
+            For i = 0 To Size - 1
+                If buf(i) = 0 Then
+                    Exit For
+                End If
+                cmd &= Chr(buf(i))
+            Next
+        Catch ex As Exception
+            MsgBox(ex.ToString, 16, "Error")
+        End Try
+        Return cmd
+    End Function
+
+    Public Function ReadData(ByVal Size As Integer) As Int32()
+        Dim data(Size) As Int32
+        data(0) = 0
+        Try
+            macc_dat.ReadArray(Of Int32)(0, data, 0, Size)
+        Catch ex As Exception
+            MsgBox(ex.ToString, 16, "Error")
+        End Try
+        Return data
+    End Function
+End Class
