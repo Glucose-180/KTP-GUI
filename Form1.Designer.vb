b309940cf9afaff4916763887846beb8ec782ab4
Glucose-180 <Glucose180@163.com>
2023-04-16 22:51:36 +0800

    Draw board

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,64 +22,125 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.SuspendLayout()
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(83, 54)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(347, 30)
-        Me.TextBox1.TabIndex = 0
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(436, 45)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(200, 48)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "w"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(436, 195)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(200, 48)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "r"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'ListBox1
-        '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 24
-        Me.ListBox1.Location = New System.Drawing.Point(83, 176)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(347, 244)
-        Me.ListBox1.TabIndex = 4
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(714, 450)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        components = New ComponentModel.Container()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        ListBox1 = New ListBox()
+        Label1 = New Label()
+        T1 = New Timer(components)
+        StatusStrip1 = New StatusStrip()
+        TSLabel1 = New ToolStripStatusLabel()
+        PB1 = New PictureBox()
+        StatusStrip1.SuspendLayout()
+        CType(PB1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(153, 49)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(244, 30)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.FlatStyle = FlatStyle.System
+        Button1.Location = New Point(425, 40)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(145, 48)
+        Button1.TabIndex = 1
+        Button1.Text = "开始计算"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.FlatStyle = FlatStyle.System
+        Button2.Location = New Point(425, 187)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(145, 48)
+        Button2.TabIndex = 3
+        Button2.Text = "r"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 24
+        ListBox1.Location = New Point(50, 168)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(347, 292)
+        ListBox1.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(47, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(100, 24)
+        Label1.TabIndex = 5
+        Label1.Text = "起始坐标："
+        ' 
+        ' T1
+        ' 
+        T1.Interval = 1000
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.ImageScalingSize = New Size(24, 24)
+        StatusStrip1.Items.AddRange(New ToolStripItem() {TSLabel1})
+        StatusStrip1.Location = New Point(0, 490)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(1270, 31)
+        StatusStrip1.TabIndex = 6
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' TSLabel1
+        ' 
+        TSLabel1.Name = "TSLabel1"
+        TSLabel1.Size = New Size(82, 24)
+        TSLabel1.Text = "等待开始"
+        ' 
+        ' PB1
+        ' 
+        PB1.Image = My.Resources.Resources.bac
+        PB1.Location = New Point(619, 7)
+        PB1.Name = "PB1"
+        PB1.Size = New Size(480, 480)
+        PB1.TabIndex = 7
+        PB1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 24F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1270, 521)
+        Controls.Add(PB1)
+        Controls.Add(StatusStrip1)
+        Controls.Add(Label1)
+        Controls.Add(ListBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "马踏棋盘"
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        CType(PB1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents T1 As Timer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents TSLabel1 As ToolStripStatusLabel
+    Friend WithEvents PB1 As PictureBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,12 +1,18 @@
-﻿Imports System.IO.MemoryMappedFiles
+﻿Imports System.ComponentModel
+Imports System.IO.MemoryMappedFiles
 Public Class Form1
+    Dim CalcTime As Integer
     Dim SmemIsOpen As Boolean
     Dim mmf_cmd, mmf_dat As MemoryMappedFile
     Dim macc_cmd, macc_dat As MemoryMappedViewAccessor
+    Dim CLI_Started As Boolean
     Const DSIZE = 64
+    Const CELLSIZE = 60
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         SmemIsOpen = False
+        DrawBoard()
+        Start_CLI()
     End Sub
 
     Public Sub Smem_Open(ByVal Name_cmd As String, ByVal Name_dat As String)
@@ -18,16 +24,43 @@ Public Class Form1
             SmemIsOpen = True
         Catch ex As Exception
             MsgBox("打开内存映射文件失败：" & vbCrLf & ex.ToString, 16, "Error")
+            If MsgBox("是否重启计算程序？", vbYesNo + vbQuestion, "") = vbYes Then
+                Kill_CLI()
+                System.Threading.Thread.Sleep(300)
+                Start_CLI()
+                System.Threading.Thread.Sleep(1000)
+            End If
             SmemIsOpen = False
         End Try
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If SmemIsOpen = False Then
-            Call Smem_Open("ktp_cmd_g", "ktp_dat_g")
-        End If
-        If SmemIsOpen = True Then
-            WriteCmd(TextBox1.Text)
+        If T1.Enabled = False Then
+            If CLI_Started = False Then
+                Button1.Text = "启动中"
+                Button1.Enabled = False
+                Start_CLI()
+                System.Threading.Thread.Sleep(2000)
+                Button1.Enabled = True
+                Button1.Text = "开始计算"
+            End If
+            If SmemIsOpen = False Then
+                Call Smem_Open("ktp_cmd_g", "ktp_dat_g")
+            End If
+            If SmemIsOpen = True Then
+                Try
+                    WriteCmd("Start " & TextBox1.Text)
+                    Button1.Text = "终止"
+                    CalcTime = 0
+                    T1.Enabled = True
+                Catch ex As Exception
+                    MsgBox(ex.ToString, 16, "Error")
+                End Try
+            End If
+        Else
+            Kill_CLI()
+            TSLabel1.Text = "已终止，共运行 " & CalcTime & " s。"
+            Button1.Text = "开始计算"
         End If
     End Sub
 
@@ -76,6 +109,7 @@ Public Class Form1
                 cmd &= Chr(buf(i))
             Next
         Catch ex As Exception
+            T1.Enabled = False
             MsgBox(ex.ToString, 16, "Error")
         End Try
         Return cmd
@@ -91,4 +125,96 @@ Public Class Form1
         End Try
         Return data
     End Function
+
+    Private Sub T1_Tick(sender As Object, e As EventArgs) Handles T1.Tick
+        Dim cmd As String
+        Dim data(DSIZE) As Int32
+        Dim i As Int32
+
+        cmd = ReadCmd(DSIZE)
+        If cmd = "Done" Then
+            T1.Enabled = False
+            data = ReadData(DSIZE)
+            ListBox1.Items.Clear()
+            For i = 0 To DSIZE - 1
+                ListBox1.Items.Add(data(i) \ 100 & vbTab & data(i) Mod 100)
+            Next
+            TSLabel1.Text = "计算完成，耗时 " & CalcTime & " s！"
+            CalcTime = 0
+            Button1.Text = "开始计算"
+        ElseIf cmd = "Error" Then
+            T1.Enabled = False
+            data = ReadData(1)
+            If data(0) = -1 Or data(0) = -2 Then
+                TSLabel1.Text = "错误：无效输入。"
+                MsgBox("错误：无效输入！", 16, "Error")
+            ElseIf data(0) = 1 Then
+                TSLabel1.Text = "计算完成，无解。"
+                MsgBox("计算完成，无解！", 64, "")
+            Else
+                TSLabel1.Text = "错误：" & data(0)
+                MsgBox("错误：代码为 " & data(0), 16, "Error")
+            End If
+            Button1.Text = "开始计算"
+        Else
+            CalcTime += 1
+            TSLabel1.Text = "计算中，已耗时 " & CalcTime & " s……"
+        End If
+    End Sub
+
+    Public Sub Start_CLI()
+        Dim Path As String
+        Try
+            Path = Application.StartupPath
+            If Path(Path.Length - 1) <> "\" Then
+                Path &= "\"
+            End If
+            Path &= "KTP-CLI.bat"
+            Shell(Path, vbMinimizedNoFocus)
+            CLI_Started = True
+        Catch ex As Exception
+            MsgBox(ex.ToString, 16, "Error")
+        End Try
+    End Sub
+
+    Public Sub Kill_CLI()
+        T1.Enabled = False
+        Smem_Close()
+        Shell("cmd /c taskkill /f /im KTP-CLI.exe", vbMinimizedNoFocus)
+        CLI_Started = False
+        SmemIsOpen = False
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles MyBase.Closing
+        Kill_CLI()
+    End Sub
+
+    Private Sub DrawBoard()
+        Dim I As Image
+        Dim G As Graphics
+        Dim P As New Pen(Color.Black)
+        Dim Br As New SolidBrush(Color.Black)
+        Dim k As Integer
+        Dim x, y As Integer
+
+        I = PB1.Image
+        G = Graphics.FromImage(I)
+        G.TranslateTransform(0, PB1.Height)
+        G.ScaleTransform(1, -1)
+        ' Draw Lines
+        For k = 1 To 7
+            G.DrawLine(P, 0, k * CELLSIZE, 8 * CELLSIZE, k * CELLSIZE)
+            G.DrawLine(P, k * CELLSIZE, 0, k * CELLSIZE, 8 * CELLSIZE)
+        Next
+        ' Fill Cells
+        For y = 0 To 7
+            For x = 0 To 7
+                If (x + y) Mod 2 = 0 Then
+                    G.FillRectangle(Br, x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE)
+                End If
+            Next
+        Next
+        G.Dispose()
+        PB1.Image = I
+    End Sub
 End Class
